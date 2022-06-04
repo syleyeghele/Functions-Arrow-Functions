86e4abfb10af96beb675b70860b67d642866a23b
Eyeghele Sylvester <syleyeghele@elatech.com.ng>
2022-06-04 14:35:28 +0000

    latest

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Stage " + num)
 console.log("")
 let userName = prompt("Username: ")
 let guessNum = Number(prompt("Guess number: "))
-let range = 0
+let range = 2
 let randomNum = Math.floor(Math.random() * range)
 let point = 0
 
@@ -12,19 +12,22 @@ if (guessNum === randomNum) {
   totalPoint = point + 1
   console.log("Congratulations" + " " + userName + ", " + "you guess right... you have" + " " + totalPoint + " point")
   totalRange = range + 1
-  console.log(totalRange)
   totalRange = range + 1
   totalStage = num + 1
-  console.log(totalStage)
 
   while (totalRange === totalRange) {
+    
   console.log("Stage " + totalStage)
+    let totalStage2 = totalStage ++
+    let totalPoint2 = totalPoint ++
   let guessNum2 = Number(prompt("Guess number: "))
-  let randomNum2 = Math.floor(Math.random() * 0)
+  let randomNum2 = Math.floor(Math.random() * totalRange)
+    let totalRange2 = totalRange ++
+    console.log(totalRange2)
     if (guessNum2 === randomNum2){
       console.log("Congratulations" + " " + userName + ", " + "you guess right... you have" + " " + totalPoint + " point")
-    }
-  console.log("lets go there...")
+    }else
+   console.log("Error, you guess wrong... Please try again!")
 }
 }
 else {
